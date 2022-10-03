2be987d236a1971d3296c99ee234e8c0e89105d5
Vatsal <v.s.gamit999@gmail.com>
2022-10-03 17:20:53 +0530

    update

--- main.py
+++ main.py
@@ -7,24 +7,28 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/test")
-def test():
+#root endpoint
+@app.get("/")
+def root():
     return JSONResponse({
-        "PING":"PONG"
+        "FastAPI+Celery":"Server is running"
     })
 
+#create your task -- !! This should be a post request
 @app.get("/create_task")
 def create_task():
     result = tasks.add.delay(2,2)
-
     return JSONResponse({
-        "result":result.id
+        "task_id":result.id,
+        "task_status":result.status
     })
 
+# get the status of your tasks by providing the id
 @app.get("/get_status/{task_id}")
 def get_status(task_id):
     result = AsyncResult(task_id)
     print(result.status)
     return JSONResponse({
+        "task_id":task_id,
         "status":result.status
     })
